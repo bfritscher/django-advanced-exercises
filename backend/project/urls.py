6ddc3bb2fddb490bcd736715692526686dec9446
Boris Fritscher <boris@fritscher.ch>
2025-03-08 23:45:27 +0100

    remove toolbar

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from shop import api
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 router = routers.DefaultRouter()
 router.register("products", api.ProductViewSet)
@@ -11,4 +10,4 @@ router.register("orders", api.OrderViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-] + debug_toolbar_urls()
+]
